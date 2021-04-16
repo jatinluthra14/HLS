30e529a20328975870a113e94aca62c6ed5bdceb
Jatin Luthra <jatinluthra14@gmail.com>
2021-04-16 21:23:13 +0530

    Add Support for possible media w/o extension (Accept)

--- app/src/main/java/com/jtb/hls/WebViewActivity.java
+++ app/src/main/java/com/jtb/hls/WebViewActivity.java
@@ -107,6 +107,7 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
                 .url(url)
                 .build();
 
+        Log.d(TAG, "Sending Media Request");
         client.newCall(request)
                 .enqueue(new Callback() {
                     @Override
@@ -119,6 +120,7 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
 
                     @Override
                     public void onResponse(Call call, final Response response) throws IOException {
+                        Log.d(TAG, "Got Media Response");
                         String req_url = response.request().url().toString();
                         String sreq_url = Util.clearQuery(req_url);
                         Map<String, Object> map = hlsStack.get(orig_url);
@@ -200,6 +202,7 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
                             String mdat = "6D646174";
 
                             in.read(buffer);
+                            Log.d(TAG, "Reading Buffer");
                             byte[] mvhd_buf = new byte[mvhd.length() / 2];
 
                             for (int i = 0; i < mvhd_buf.length; i++) {
@@ -208,7 +211,7 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
                                 mvhd_buf[i] = (byte) j;
                             }
                             mvhd_idx = Util.indexOf(buffer, mvhd_buf);
-
+                            Log.d(TAG, "Got MVHD Index");
                             if (mvhd_idx == -1) {
                                 byte[] mdat_buf = new byte[mdat.length() / 2];
                                 for (int i = 0; i < mvhd_buf.length; i++) {
@@ -218,6 +221,7 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
                                 }
 
                                 int mdat_idx = Util.indexOf(buffer, mdat_buf);
+                                Log.d(TAG, "Got MDAT Index");
                                 if (mdat_idx != -1) {
                                     StringBuilder sb = new StringBuilder();
                                     for (int i=mdat_idx - 4; i < mdat_idx; i++) {
@@ -240,6 +244,7 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
                             }
 
                             mvhd_idx += 17;
+                            Log.d(TAG, "Got MVHD Index 2");
                             StringBuilder sb = new StringBuilder();
                             for (int i=mvhd_idx; i<mvhd_idx+3; i++) {
                                 byte b = buffer[i];
@@ -262,6 +267,83 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
                             map.put("duration", formatted_duration);
                             map.put("type", "mp4");
                             hlsStack.put(orig_url, map);
+                        } else {
+                            try {
+                                BufferedInputStream in = new BufferedInputStream(response.body().byteStream(), 128);
+                                byte[] buffer = new byte[128];
+                                int mvhd_idx = -1;
+                                String mvhd = "6D766864";
+                                String mdat = "6D646174";
+
+                                in.read(buffer);
+                                Log.d(TAG, "Reading Buffer");
+                                byte[] mvhd_buf = new byte[mvhd.length() / 2];
+
+                                for (int i = 0; i < mvhd_buf.length; i++) {
+                                    int index = i * 2;
+                                    int j = Integer.parseInt(mvhd.substring(index, index + 2), 16);
+                                    mvhd_buf[i] = (byte) j;
+                                }
+                                mvhd_idx = Util.indexOf(buffer, mvhd_buf);
+                                Log.d(TAG, "Got MVHD Index");
+                                if (mvhd_idx == -1) {
+                                    byte[] mdat_buf = new byte[mdat.length() / 2];
+                                    for (int i = 0; i < mvhd_buf.length; i++) {
+                                        int index = i * 2;
+                                        int j = Integer.parseInt(mdat.substring(index, index + 2), 16);
+                                        mdat_buf[i] = (byte) j;
+                                    }
+
+                                    int mdat_idx = Util.indexOf(buffer, mdat_buf);
+                                    Log.d(TAG, "Got MDAT Index");
+                                    if (mdat_idx != -1) {
+                                        StringBuilder sb = new StringBuilder();
+                                        for (int i=mdat_idx - 4; i < mdat_idx; i++) {
+                                            byte b = buffer[i];
+                                            sb.append(String.format("%02X", b));
+                                        }
+
+                                        int mdat_len = Integer.parseInt(sb.toString(), 16);
+                                        in.skip(mdat_len - 128);
+                                        in.read(buffer);
+
+                                        sb = new StringBuilder();
+                                        for (int i=0; i<buffer.length; i++) {
+                                            byte b = buffer[i];
+                                            sb.append(String.format("%02X", b));
+                                        }
+
+                                        mvhd_idx = Util.indexOf(buffer, mvhd_buf);
+                                    }
+                                }
+
+                                mvhd_idx += 17;
+                                Log.d(TAG, "Got MVHD Index 2");
+                                StringBuilder sb = new StringBuilder();
+                                for (int i=mvhd_idx; i<mvhd_idx+3; i++) {
+                                    byte b = buffer[i];
+                                    sb.append(String.format("%02X", b));
+                                }
+
+                                int time_scale = Integer.parseInt(sb.toString(), 16);
+                                sb = new StringBuilder();
+                                for (int i=mvhd_idx+4; i<mvhd_idx+7; i++) {
+                                    byte b = buffer[i];
+                                    sb.append(String.format("%02X", b));
+                                }
+
+                                int scaled_duration = Integer.parseInt(sb.toString(),16);
+                                Double total_duration = ((double)scaled_duration) / ((double)time_scale);
+                                String formatted_duration = DateUtils.formatElapsedTime(total_duration.longValue());
+                                Log.d(TAG, "Orig URL: " + orig_url);
+
+                                map = hlsStack.get(orig_url);
+                                map.put("duration", formatted_duration);
+                                map.put("type", "mp4");
+                                hlsStack.put(orig_url, map);
+                            } catch (Exception e) {
+                                Log.e(TAG, e.toString());
+                            }
                         }
                         stream_flag = 0;
                     }
@@ -503,7 +585,8 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
                     String lpath = path.toLowerCase();
                     allRequests.add(path);
                     Log.d(RequestTAG, path);
-                    if (spath.endsWith(".m3u8") || spath.endsWith(".mp4") || spath.endsWith(".vid") && !(spath.endsWith(".ts"))) {
+//                    for (String header_key : request.getRequestHeaders().keySet()) Log.d(RequestTAG, header_key + ": " + request.getRequestHeaders().get(header_key));
+                    if (spath.endsWith(".m3u8") || spath.endsWith(".mp4") || spath.endsWith(".vid") && !(spath.endsWith(".ts")) || (request.getRequestHeaders().containsKey("Range"))) {
                         if (!(hlsStack.containsKey(path))) {
                             Log.d(TAG, "New Media Url: " + path);
                             resolveMedia(path);
